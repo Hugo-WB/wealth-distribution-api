77e8ed7f42343f082e991e42db27f2005bb5fccc
hugo-wb <hugowong71@gmail.com>
2020-11-09 16:32:51 +0000

    final

--- python/data.py
+++ python/data.py
@@ -6,18 +6,35 @@ import math
 url = "http://localhost:8080/data/"
 
 
-class 
-def uploadFile(fileLink):
-    try:
-        file = pd.read_csv(fileLink,sep=";")
-        print(file)
-    except Exception as error:
-        print(error)
-        return
+class file:
+    def __init__(self,link):
+        self.json = {}
+        self.fileLink = link
+        if not(self.openFile()):
+            return
+        
+        self.findQuartilesPercentages()
+
+    
+    def findQuartilesPercentages(self):
+        for row in self.file.iterrows():
+            if row[]
+
+    # Will open file from file link
+    def openFile(self):
+        try:
+            self.file = pd.read_csv(self.fileLink,sep=";")
+            return True
+        except Exception as error:
+            print(error)
+            return False
+        
+    # upload json to url
+    def upload(self):
+        pass
 
 
-
-
-result = requests.get("http://localhost:8080/countries")
+# countryCodes = requests.get("http://localhost:8080/countries")["code"]
+countryCodes = ["AD"]
 for result in result.json():
-    uploadFile("../wid_all_data/WID_data_"+result["code"]+".csv")
+    current = file("../wid_all_data/WID_data_"+countryCodes+".csv")
